2290bc9815760bc65d268a3ec2acbc7c8276ff10
zorangblax <fahrezagants34@gmail.com>
2023-10-17 11:38:15 +0700

    pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama : Muhammad Fahreza</li>
+ </ul>
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> Jalan Durian no. 41 Munjuk</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Munjuk</li>
+ </ul>
+
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+    <li>Bulutangkis</li>
+ </ul>
+</section>
---
+++ application/views/view-data-matakuliah.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>Tampil Data Matakuliah</title>
+    </head>
+    <body>
+        <center>
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Tampil Data Matakuliah
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="3">
+                        <hr>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        Kode MTK
+                    </th>
+                    <th>:</th>
+                    <td>
+                        <?=$kode;?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Nama MTK
+                    </th>
+                    <th>:</th>
+                    <td>
+                        <?=$nama;?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        SKS
+                    </th>
+                    <th>:</th>
+                    <td>
+                        <?=$sks;?>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">
+                        <a href="<?= base_url('matakuliah');?>">Kembali</a>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </body>
+</html>
